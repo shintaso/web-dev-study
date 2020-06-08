6cb62c2ca460a133c1a8abc93aa96f198ec5b476
rs617 <r6170.s@gmail.com>
2020-06-08 18:11:49 +0900

    また変更したんだよ

---
+++ nattyo.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('h1').hover(
+        function(){
+            $('h1').fadeIn(1500);
+
+        },
+        function(){
+            $('h1').fadeOut(1500);
+
+        });
+
+
+});
